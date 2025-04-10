3ea537617278e270840ef408b8a4b707e83b81e0
Fari Noveri <cahayalunamaharani1@gmail.com>
2025-04-10 15:14:16 +0700

    Add css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  incrementAsync,
+  selectCount,
+} from './features/counter/counterslice'
+
+import './App.css'
 
 function App() {
+  const count = useSelector(selectCount)
+  const dispatch = useDispatch()
+  const [incrementAmount, setIncrementAmount] = useState('2')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1>Redux Counter</h1>
+      <h2>{count}</h2>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <div className="controls">
+        <input
+          type="number"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button
+          onClick={() =>
+            dispatch(incrementByAmount(Number(incrementAmount) || 0))
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Add Amount
+        </button>
+        <button
+          onClick={() =>
+            dispatch(incrementAsync(Number(incrementAmount) || 0))
+          }
+        >
+          Add Async
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
